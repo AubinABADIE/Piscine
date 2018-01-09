d96c09c75df8b331c8d19f17155ff0a4b526505d
ysanson <yvan.sanson@etu.umontpellier.fr>
2018-01-09 13:46:48 +0100

    Modification editor.js

--- assets/javascript/editors.js
+++ assets/javascript/editors.js
@@ -5,13 +5,3 @@ $("#delete-editor-btn").click(function () {
       console.log("delete")
   }
 });
-
-
-function stopRKey(evt) {
-    var evt = (evt) ? evt : ((event) ? event : null);
-    var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
-    if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
-  }
-  
-  document.onkeypress = stopRKey;
-  
