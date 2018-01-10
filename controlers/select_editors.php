34a223c62f3eb4f70e6aab5842b3d12967ddaf68
PaulArnaud <32633519+PaulArnaud@users.noreply.github.com>
2018-01-10 15:32:17 +0100

    Update select_editors.php

--- controlers/select_editors.php
+++ controlers/select_editors.php
@@ -1,1 +1,18 @@
+<?php
+		require('connect_bdd.php');
 
+		$InfoEditeur = $BaseDeDonnée->query('select ID_Editor,Name,count(ID_Game) from editor');
+		$data = $InfoEditeur->fetchAll();
+		$TABLEAU_FINAL = array()
+		foreach ($data as $key => $value) 
+		{
+			$TABLEAU_INDIV=array([,,])
+			$TABLEAU_INDIV[0]='ID_Editor'
+			$TABLEAU_INDIV[1]='Name'
+			$TABLEAU_INDIV[2]='count(ID_Game'
+			echo <tbody> $TABLEAU_INDIV <tbody>
+
+		}
+		$InfoEditeur->closeCursor();
+		$InfoEditeur = null;
+?>
