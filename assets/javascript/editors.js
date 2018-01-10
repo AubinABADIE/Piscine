706da0151859ce5f911c7edad4315f04404bb26c
AlexandreKueny <alexandre.kueny@hotmail.com>
2018-01-11 00:23:29 +0100

    Add visualisation for selected line

--- assets/javascript/editors.js
+++ assets/javascript/editors.js
@@ -1,3 +1,5 @@
+var selected_id;
+
 $("#delete-editor-btn").click(function () {
     if (confirm("Confirmer la suppression ?")) {
         console.log("delete")
@@ -5,7 +7,9 @@ $("#delete-editor-btn").click(function () {
 });
 
 $("tr").click(function () {
-    var selected_id = $(this).attr('id')
-    $("#show-editor-btn").attr("href", "show.php?id=" + selected_id)
-    $("#edit-editor-btn").attr("href", "edit.php?id=" + selected_id)
+    $("#" + selected_id).removeClass("selected");
+    selected_id = $(this).attr('id');
+    $(this).addClass("selected");
+    $("#show-editor-btn").attr("href", "show.php?id=" + selected_id);
+    $("#edit-editor-btn").attr("href", "edit.php?id=" + selected_id);
 });
