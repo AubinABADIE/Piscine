0153550beadc576692e7e66ad448adc19320a2d3
PaulArnaud <32633519+PaulArnaud@users.noreply.github.com>
2018-01-10 12:43:41 +0100

    Update select_editors.php

--- controlers/select_editors.php
+++ controlers/select_editors.php
@@ -1,16 +1,1 @@
-<?php
 
-    require('connect_bdd.php');
-
-    $stmt = $pdo->query('SELECT * FROM editor;');
-    $data = $stmt->fetchAll();
-
-    foreach ($data as $key => $value) {
-        echo '<div>'.$value['Value1'].'</div>';
-        echo '<div>'.$value['Value2'].'</div>';
-        echo '<div>'.$value['Value3'].'</div>';
-    }
-
-    $stmt->closeCursor();
-    $stmt = null;
-?>
