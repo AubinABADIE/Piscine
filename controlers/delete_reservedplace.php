c191ef74cbc340b5eca2ebcea03bcd34ef5de762
Aubin ABADIE <aubin.abadie@gmail.com>
2018-01-14 19:43:40 +0100

    update delete

--- controlers/delete_reservedplace.php
+++ controlers/delete_reservedplace.php
@@ -4,11 +4,11 @@
 
     $id = $_GET['id'];
 
-    $result = $bdd->query('SELECT ID_Booking FROM lodge l INNER JOIN lodgment lo ON (l.ID_Lodgment = lo.ID_Lodgment) WHERE ID_Lodgment = "'.$id.'"');
+    $result = $bdd->query('SELECT ID_Booking FROM reserved_place r INNER JOIN lodgment l ON (r.ID_Space = l.ID_Space) WHERE ID_Space = "'.$id.'"');
     $data = $result->fetchAll(PDO::FETCH_ASSOC);
     unset($result);
     
-    $result = $bdd->query('DELETE FROM lodge WHERE ID_Lodgment = "'.$id.'"');
+    $result = $bdd->query('DELETE FROM reserved_place WHERE ID_Space = "'.$id.'"');
     unset($result);
 
     foreach ($data as $value) {
