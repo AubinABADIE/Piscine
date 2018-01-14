6e36a047eeef9384000d8dbe89ae52a546cdb252
AlexandreKueny <alexandre.kueny@hotmail.com>
2018-01-14 18:22:34 +0100

    Link delete btn

--- controlers/delete_games.php
+++ controlers/delete_games.php
@@ -4,12 +4,16 @@
 
     $id = $_GET['id'];
 
+    $result = $bdd->query('SELECT ID_Editor FROM game WHERE ID_Game = "'.$id.'"');
+    $data = $result->fetchAll(PDO::FETCH_ASSOC);
+    unset($result);
+
     $result = $bdd->query('DELETE FROM game WHERE ID_Game = "'.$id.'"');
     unset($result);
 
     $result = $bdd->query('DELETE FROM reserved_place WHERE ID_Game = "'.$id.'"');
     unset($result);
 
-    header("Location: ../views/editors/index.php");
+    header('Location: ../views/editors/show.php?id='.$data[0]['ID_Editor']);
     exit();
 ?>
